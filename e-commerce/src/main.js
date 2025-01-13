0e61ad5edfb4fedefe7737da6764b2dedce6ba5e
ChristoNP <chris.pontoh@gmail.com>
2025-01-13 15:48:49 +0700

    main.js change

--- e-commerce/src/main.js
+++ e-commerce/src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import CategoriesView from './views/CategoriesView.vue'
+import ProductsView from './views/ProductsView.vue'
+import { createPinia } from 'pinia'
+import { apolloClient } from './apollo/client'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: CategoriesView},
+        {path: '/products', component: ProductsView}
+    ]
+})
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(apolloClient)
+app.mount('#app')
